2493204bae91400f937e309345589d5dd5afda9d
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-05-15 12:20:43 +0200

    New project and touch controlls

---
+++ Assets/Scripts/ImageSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSelection : MonoBehaviour {
+
+    public float waitTime = 2f;
+    
+    private Renderer rend;
+    private bool isSelected;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseDown() {
+        if (!isSelected) {
+            isSelected = true;
+            rend.material.SetFloat("_OutlineEnabled", 1);
+            StartCoroutine(OutlineWait());
+        }
+        else {
+            Debug.Log("Open Info");
+        }
+    }
+
+    private IEnumerator OutlineWait() {
+        yield return new WaitForSecondsRealtime(waitTime);
+        rend.material.SetFloat("_OutlineEnabled", 0);
+        isSelected = false;
+    }
+}
---
+++ Assets/Scripts/TouchControlls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControlls : MonoBehaviour {
+
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 10;
+
+    private Vector3 touchStart;
+    private bool isDragging;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isDragging = true;
+        }
+
+        if(Input.touchCount == 2) {
+            isDragging = false;
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            Zoom(difference * 0.01f);
+        }
+        else if (Input.touchCount == 1 && isDragging) {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+    }
+
+    private void Zoom(float increment) {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+
+}
